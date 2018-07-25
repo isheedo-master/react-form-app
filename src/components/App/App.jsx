23ffeaa30e26d54860c6ecc1063a18d62d4d4d07
Ivan Sumlikin <isheedo@gmail.com>
2018-07-25 23:26:49 +0700

    Initial project setup

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>
+          Hi there
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const AppRouter = () => (
+  <Router>
+    <Route path="/" component={App} />
+  </Router>
+);
+
+const appElement = document.getElementById('app');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
+  appElement,
+);
+
+module.hot.accept();
